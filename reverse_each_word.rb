57aeea36e20985827f4569030a0e5fd5f79a9516
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-04 16:29:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   array = []
-  array << sentence
-  array.collect do |word|
-    word.reverse
-  end
+  array << sentence.split
+  array.collect {|word| word.reverse}
 end
+
